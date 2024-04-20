9d5c3bd4f5f04ac92ed4145f5f5482e8ec6be62d
Seacrest <paganseacrest@gmail.com>
2024-04-20 22:00:28 +0300

    0.0.1

--- src/lib.ts
+++ src/lib.ts
@@ -1,8 +1,3 @@
-import { ex, getRef, ref } from "./common";
-import { match } from "./match";
-import { Some } from "./option";
-import { Err, Ok, Result } from "./result";
-
 export * from "./cmp";
 export * from "./common";
 export * from "./match";
