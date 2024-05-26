890bff5246edb2cf4b661955f9a36ddb957efd9c
Seacrest <paganseacrest@gmail.com>
2024-05-26 14:04:00 +0300

    v0.0.23 - async channels

--- src/sync.ts
+++ src/sync.ts
@@ -1,12 +1,42 @@
-import { isValue } from "./core";
+import { Sized, isValue } from "./core";
 import { Option } from "./option";
 import { Err, Ok, Result } from "./result";
 
-export class SyncSenderError {
+export class SenderError {
     error = "";
 
-    constructor(impl: (self: SyncSenderError) => void) {
+    constructor(impl: (self: SenderError) => void) {
         impl(this);
+    }
+}
+
+export class ReceiverError<E> implements Sized<E> {
+    $ref: [E] = [ReceiverError.name as E];
+    variant = [ReceiverError.name, null as E];
+
+    #variant(fn: Function, err: E) {
+        this.$ref[0] = err;
+        this.variant = [fn.name, err];
+        Object.freeze(this);
+
+        return this;
+    }
+
+    empty() {
+        return this.#variant(this.empty, null as E);
+    }
+
+    error(err: E) {
+        return this.#variant(this.error, err)
+    }
+
+    static Empty<E>() {
+        return new ReceiverError<E>().empty();
+    }
+
+
+    static Error<E>(err: E) {
+        return new ReceiverError<E>().error(err);
     }
 }
 
@@ -21,12 +51,12 @@ export class SyncSender<T> {
     impl(this);
   }
   
-  send(value: T): Result<{}, SyncSenderError> {
+  send(value: T): Result<{}, SenderError> {
     if (isValue(value)) {
         this.#messages.unshift(value);
         return Ok({})
     } else {
-        return Err(new SyncSenderError(self => {
+        return Err(new SenderError(self => {
             self.error = "No valid value has been sent";
         }));
     }
@@ -56,4 +86,56 @@ export function syncChannel<T>(): [SyncSender<T>, SyncReceiver<T>] {
   const rx = new SyncReceiver<T>(self => void (self.messages = messages));
 
   return [tx, rx];
+}
+
+export class Sender<T> {
+    tasks: Promise<Promise<T>[]> = Promise.resolve([]);
+
+    constructor(impl: (self: Sender<T>) => void) {
+        impl(this);
+    }
+    
+    send(task: Promise<T>) {
+        this.tasks.then(t => {
+            t.unshift(task);
+        });
+    }
+}
+
+export class Receiver<T> {
+    tasks: Promise<Promise<T>[]> = Promise.resolve([]);
+
+    constructor(impl: (self: Receiver<T>) => void) {
+        impl(this);
+    }
+
+    recv<E = unknown>(): Promise<Result<T, ReceiverError<E>>> {
+        return this.tasks
+            .then(t => {
+                const task = t.pop();
+
+                if (!task) {
+                    return Err<ReceiverError<E>, T>(ReceiverError.Empty())
+                }
+
+                return task.then(t => {
+                    return Ok<T, ReceiverError<E>>(t)
+                }).catch(err => Err<ReceiverError<E>, T>(ReceiverError.Error(err)))
+                
+            }).catch(err => Err<ReceiverError<E>, T>(ReceiverError.Error(err)));
+    }
+}
+
+export function channel<T>(): [Sender<T>, Receiver<T>] {
+    const tasks: Promise<Promise<T>[]> = Promise.resolve([]);
+    
+    const tx = new Sender<T>(self => {
+        self.tasks = tasks;
+    });
+
+    const rx = new Receiver<T>(self => {
+        self.tasks = tasks;
+    });
+    
+    return [tx, rx];
 }
